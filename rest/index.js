bc8149a01eb54a22fd6db202c12f4f85a96c394f
Benjamin Schultz Larsen (EDU) <benjaco1996+edu@gmail.com>
2019-09-26 11:10:39 +0200

    Update index.js

--- rest/index.js
+++ rest/index.js
@@ -35,29 +35,35 @@ app.use(bodyParser.json());
     app.get('/students', async (req, res) => {
         res.json(await db.query`select * from students`)
     });
+    
     app.post('/students', async (req, res) => {
         console.log(req.body)
         await db.query`insert into students (name, age) values (${req.body.name}, ${req.body.age})`;
         res.status(201).end();
     });
+    
     app.get('/students/:id', async (req, res) => {
         let data = await db.query`select * from students where id = ${req.params.id}`
         if(data.length === 0)
             return res.status(404).end();
         res.json(data[0]);
     });
+    
     app.put('/students/:id', async (req, res) => {
         await db.query`update students set name = ${req.body.name}, age = ${req.body.age} where id = ${req.params.id}`;
         res.status(202).end();
     });
+    
     app.delete('/students/:id',async (req, res) => {
         let data = await db.query`select * from students where id = ${req.params.id}`
-        if(data.length === 0){
+        if(data.length === 0)
             return res.status(404).end();
-        }
         await db.query`delete from students where id = ${req.params.id}`
         res.status(202).end();
     });
+    
+    
+    
     app.listen(8080, () =>
         console.log(`Example app listening on port 8080`),
     );
